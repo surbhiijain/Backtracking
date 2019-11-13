2b9fd66cd6314197cfe79739e161fb9c97fc3374
surbhiijain <30298846+surbhiijain@users.noreply.github.com>
2019-11-13 10:36:25 -0800

    board functionalist draft complete
    not debugged

--- src/backtracking/Board.java
+++ src/backtracking/Board.java
@@ -45,11 +45,65 @@ public class Board {
         return board[row][col];
     }
 
-    public boolean checkRow(int row) {
-        return false;
+    private boolean checkSet(int[] set, int leftHint, int rightHint) {
+        // check for uniqueness
+        boolean[] seen = new boolean[size];
+        for (int i = 0; i < seen.length; i++) {
+            if (set[i] == 0) {
+                continue;
+            }
+            if (seen[set[i] - 1]) {
+                return false;
+            }
+            seen[set[i] - 1] = true;
+        }
+        if (leftHint != 0) {
+            // count from left
+            int maxSeen = 0;
+            int count = 0;
+            for (int i = 0; i < set.length; i++) {
+                if (set[i] == 0) {
+                    return true; // all partial sets are set as true
+                }
+                if (set[i] > maxSeen) {
+                    count++;
+                    maxSeen = set[i];
+                }
+            }
+            if (count != leftHint) {
+                return false;
+            }
+        }
+        if (rightHint != 0) {
+            int maxSeen = 0;
+            int count = 0;
+            for (int i = set.length - 1; i >= 0; i--) {
+                if (set[i] == 0) {
+                    return true; // all partial sets are set as true
+                }
+                if (set[i] > maxSeen) {
+                    count++;
+                    maxSeen = set[i];
+                }
+            }
+            if (count != rightHint) {
+                return false;
+            }
+            // count from right
+        }
+        return true;
     }
 
-    public boolean checkCol() {
-        return false;
+    public boolean checkRow(int row) {
+        return checkSet(board[row], this.getLeftConstraint(row), this.getRightConstraint(row));
+    }
+
+    public boolean checkCol(int col) {
+        int[] set = new int[size];
+        for (int i = 0; i < set.length; i++) {
+            set[i] = board[i][col];
+        }
+        return checkSet(set, this.getLeftConstraint(col), this.getRightConstraint(col));
+
     }
 }
